c4322c2fad924699e9a54ff25acb968108e0798e
John Martel <jonathan.martel@coglinc.ca>
2020-04-22 21:23:53 -0400

    Configure jest with ts-jest

---
+++ jest.config.js
@@ -0,0 +1,18 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+  collectCoverage: true,
+  collectCoverageFrom: ['src/**/*.ts', '!lib/**', '!node_modules/**'],
+  coverageDirectory: 'coverage',
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100,
+    },
+  },
+  moduleFileExtensions: ['js', 'ts', 'json'],
+  preset: 'ts-jest',
+  reporters: ['default', 'jest-junit'],
+  testEnvironment: 'node',
+};
